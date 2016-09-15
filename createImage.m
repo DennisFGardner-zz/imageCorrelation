bd812daefffce2d9310b8b4cfad0521287da2260
Dennis Gardner <dennis.gardner@colorado.edu>
2016-09-15 10:31:23 -0700

    square creation, minor changes to help file

--- createImage.m
+++ createImage.m
@@ -8,18 +8,18 @@ function [I] = createImage(M, N, W, type)
 % Syntax:  [I] = createImage(M,N,W, type);
 %
 % Inputs:
-%    M - rows
-%    N - cols
-%    W - width
+%    M - rows (pixels)
+%    N - cols (pixels)
+%    W - width (pixels)
 %    type - string - 'square' or 'circle'
 %
 % Outputs:
 %    I - binary array with either a square or circle in the middle
 %
 % Example: 
-%    [I] = createImage(512, 512, 256, 'square'); imagesc(I);
+%    [I] = createImage(512, 512, 128, 'square'); imagesc(I);
 %    or
-%    [I] = createImage(512, 512, 256, 'circle'); imagesc(I);
+%    [I] = createImage(512, 512, 128, 'circle'); imagesc(I);
 %
 % Other m-files required: none
 % Subfunctions: none
@@ -36,8 +36,17 @@ function [I] = createImage(M, N, W, type)
 
 %------------- BEGIN CODE --------------
 
-% Enter your executable matlab commands here
+I = zeros(M, N);
 
+switch type
+    case 'square'
+        I(M/2-W/2:M/2+W/2-1, N/2-W/2:N/2+W/2-1) = 1;
+        
+    case 'circle'
+        
+end
+
+I = logical(I);
 
 %------------- END OF CODE --------------
 end
