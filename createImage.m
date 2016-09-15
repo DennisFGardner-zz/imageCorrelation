4f1d68048b703b4faeadb202056f92820840b701
Dennis Gardner <dennis.gardner@colorado.edu>
2016-09-15 09:51:10 -0700

    created function createImage

---
+++ createImage.m
@@ -0,0 +1,42 @@
+function [I] = createImage(M, N, W, 'type')
+%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+
+% Author: Dennis F Gardner
+% JILA, Univeristy of Colorado, 440 UCB, Boulder, CO 80309
+% email: dennis.gardner@colorado.edu
+% Website 1: http://www.github.com/DennisFGardner 
+% Website 2: http://www.linkedin.com/in/dennisfgardner
+% File Creation: Sept. 14th, 2016
+
+%------------- BEGIN CODE --------------
+
+% Enter your executable matlab commands here
+
+
+%------------- END OF CODE --------------
+end
