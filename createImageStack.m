be0a5e90e02c2944901fc188aad9c6b81d35aa9e
Dennis Gardner <dennis.gardner@colorado.edu>
2016-09-15 13:39:13 -0700

    comments and whitespace for readiblity

--- createImageStack.m
+++ createImageStack.m
@@ -1,35 +1,33 @@
-function [stackI] = createImageStack(I,L,type,varargin)
+function [stackI] = createImageStack(I, L, type, varargin)
 %CREATEIMAGESTACK - create 3D array, each slice is I with a circshift
 %   This function will create a 3D array. Each slice of the array will
 %   contain the original image, I, but it will be shifited by an interger
 %   number of pixels. The first slice, stackI(:,:,1), will be unshifted.
 %
+% Syntax:   [stackI] = createImageStack(I, L, type, parameter1, parameter2);
+%           [stackI] = createImageStack(I, L, 'sinx', PERIOD, AMP);
+%           [stackI] = createImageStack(I, L, 'siny', PERIOD, AMP);
+%
 % Inputs:
 %    I - original 2D image size [M, N]
 %    L - number of slices in the 3D array
-%    type - type of motions: sinx, (more coming in the future)
+%    type - type of motions: sinx, siny, (more coming in the future)
 %       sinx - left/right sinusoidal motion
 %       siny - up/down sinusoidal motion 
 %
 % Outputs:
 %    stackI - 3D array size [M, N, L], each slice is a shifted I
-%
-% Syntax:   [stackI] = createImageStack(I, L, type, parameter1, parameter2);
-%           [stackI] = createImageStack(I, L, 'sinx', PERIOD, AMP);
-%           [stackI] = createImageStack(I, L, 'siny', PERIOD, AMP);
-%
 %           
 % Example:
-%       [I] = createImage(512, 512, 128, 'square'); imagesc(I); axis image;
-%       L = 50; period = 25; amp = 100;
-%       [stackI] = createImageStack(I, L, 'sinx', period, 100);
-%       for ii=1:L, 
-%          imagesc(stackI(:,:,ii)); 
-%          drawnow; 
-%          pause(0.05); 
-%       end;
-%       close all
-%       
+%     [I] = createImage(512, 512, 128, 'square');
+%     L = 50; period = 25; amp = 100;
+%     [stackI] = createImageStack(I, L, 'sinx', period, 100);
+%     for ii=1:L, 
+%         imagesc(stackI(:,:,ii)); axis image
+%         drawnow; 
+%         pause(0.05); 
+%     end;
+%     close all
 %
 % Other m-files required: none
 % Subfunctions: none
@@ -46,15 +44,23 @@ function [stackI] = createImageStack(I,L,type,varargin)
 
 %------------- BEGIN CODE --------------
 
+% size of the original image
 [M, N] = size(I);
+
+% initialize the stackI
 stackI = zeros(M, N, L);
 
+% determine the kind of motion (right now it's only sine, but in the future
+% there could be more)
 typeClass = type(1:3);
 
 switch typeClass
     case 'sin'
+        % parameters for sin motion
         period = varargin{1};
         amp = varargin{2};
+        
+        % create sinusoidal shift motion
         x = 0:1:L-1;
         shifts = amp*sin(2*pi*x/period);
         
@@ -62,26 +68,25 @@ switch typeClass
         shifts = round(shifts);
         
         if strcmp(type, 'sinx')
-            % shift along the col direction (left-right)
+            % shift along the col direction (left/right)
             for ii = 1:L,
                 stackI(:,:,ii) = circshift(I, shifts(ii), 2);
             end
             
         elseif strcmp(type, 'siny');
-            % shift along the col direction (left-right)
+            % shift along the row direction (up/down)
             for ii = 1:L,
                 stackI(:,:,ii) = circshift(I, shifts(ii), 1);
             end
             
         else
-            error(message('sin type not recognized'))
+            error('sin type not recognized')
         end
 
     otherwise
-        error(message('type not supported'))
+        error('type not supported')
         
 end
-
 
 %------------- END OF CODE --------------
 end
