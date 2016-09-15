a721301ff20dc2fb1c8d65ec830c70e44f6e6548
Dennis Gardner <dennis.gardner@colorado.edu>
2016-09-15 11:40:52 -0700

    most help file info

--- createImageStack.m
+++ createImageStack.m
@@ -1,19 +1,18 @@
-function [output1,output2] = function_name(input1,input2,input3)
-%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
-%   Optional file header info (to give more details about the function than in the H1 line)
-%   Optional file header info (to give more details about the function than in the H1 line)
-%   Optional file header info (to give more details about the function than in the H1 line)
+function [stackI] = createImageStack(I,L,type,varargin)
+%CREATEIMAGESTACK - create 3D array, each slice is I with a circshift
+%   This function will create a 3D array. Each slice of the array will
+%   contain the original image, I, but it will be shifited by an interger
+%   number of pixels. 
 %
-% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+% Syntax:  [stackI] = createImageStack(I, L, type, parameter1, parameter2)
 %
 % Inputs:
-%    input1 - Description
-%    input2 - Description
-%    input3 - Description
+%    I - original 2D image size [M, N]
+%    L - number of slices in the 3D array
+%    type - type of motions: sinusoidal, (more coming in the future)
 %
 % Outputs:
-%    output1 - Description
-%    output2 - Description
+%    stackI - 2D array size [M, N, L], each slice is a shifted I
 %
 % Example: 
 %    Line 1 of example
@@ -24,14 +23,14 @@ function [output1,output2] = function_name(input1,input2,input3)
 % Subfunctions: none
 % MAT-files required: none
 %
-% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+% See also: CREATEIMAGE
 
 % Author: Dennis F Gardner
 % JILA, Univeristy of Colorado, 440 UCB, Boulder, CO 80309
 % email: dennis.gardner@colorado.edu
 % Website 1: http://www.github.com/DennisFGardner 
 % Website 2: http://www.linkedin.com/in/dennisfgardner
-% File Creation: Sept. 14th, 2016
+% File Creation: Sept. 15th, 2016
 
 %------------- BEGIN CODE --------------
 
