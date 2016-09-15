7b150a46902fb76182e9f8dda5731bb315ce9aa1
Dennis Gardner <dennis.gardner@colorado.edu>
2016-09-15 14:09:40 -0700

    creation of MAIN script
    I like to use the cell feature in MATLAB scripts. I am creating a MAIN script here that will be used to run the test data on imageCorrelation

---
+++ MAIN.m
@@ -0,0 +1,10 @@
+%% Main Script for imageCorrelation
+%
+% Author: Dennis F Gardner
+% JILA, Univeristy of Colorado, 440 UCB, Boulder, CO 80309
+% email: dennis.gardner@colorado.edu
+% Website 1: http://www.github.com/DennisFGardner 
+% Website 2: http://www.linkedin.com/in/dennisfgardner
+% File Creation: Sept. 14th, 2016
+
+%%
