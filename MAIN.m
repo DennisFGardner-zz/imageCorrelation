8d44faa40e27f3fe882888bff6c749702ca2d8ba
Dennis Gardner <dennis.gardner@colorado.edu>
2016-09-16 09:38:07 -0700

    large amp little width example added

--- MAIN.m
+++ MAIN.m
@@ -26,5 +26,23 @@ gamma = imageCorrelation(I, stackI); plot(gamma); axis tight;
 % i like a clean Workspace
 clear L period amp ii
 
+%% What happens with the amplitude is larger than the width?
 
+[I] = createImage(512, 512, 50, 'square');
+L = 50; period = 50; amp = 100;
+[stackI] = createImageStack(I, L, 'sinx', period, amp);
+for ii=1:L, 
+    imagesc(stackI(:,:,ii)); axis image
+    drawnow; 
+    pause(0.05); 
+end;
+close all
 
+gamma = imageCorrelation(I, stackI); plot(gamma,'.'); axis tight;
+
+% i like a clean Workspace
+clear L period amp ii
+
+%% Summary of tests
+% 1 means image is exactly the same
+% 0 means no overlap between images
